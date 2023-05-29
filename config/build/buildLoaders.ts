c69806f08be7dac9292a62c41ae91f525705da5e
zhdanov <zhdanov@treaton.ru>
2023-05-30 01:15:00 +0500

    Начал конфиг вебпака

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,30 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
+
+  const lessLoader = {
+      test: /\.less$/i,
+      use: [
+        options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: {
+            modules: true
+          }
+        },
+        "less-loader",
+      ],
+    }
+
+  return [
+    typescriptLoader,
+    lessLoader
+  ]
+}
---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import webpack from 'webpack';
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildMode, BuildPaths} from "./config/build/types/config";
+
+export default (env: BuildEnv): webpack.Configuration => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  }
+
+  const mode: BuildMode = env.mode || "development";
+  const isDev: boolean = mode === "development";
+  const PORT: number = env.port || 3000;
+
+  return buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT
+  });
+};
