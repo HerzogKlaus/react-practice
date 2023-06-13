4e77ba4ae809b68f980438074b86c6724e38d2eb
HerzogKlaus <maxspace2000@mail.ru>
2023-06-14 01:27:28 +0500

    понизил версию i18n

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -3,23 +3,18 @@ import {initReactI18next} from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init(() => ({
+  .init({
     fallbackLng: 'ru',
     debug: IS_DEV,
 
     interpolation: {
       escapeValue: false,
     },
-
-    backend: {
-      loadPath: '/locales/{{lng}}.json'
-    }
-  }));
+  });
 
 
 export default i18n;
