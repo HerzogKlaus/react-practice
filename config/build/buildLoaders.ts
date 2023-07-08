5e701b543e2a140768f8edb6d5a27394a9a4c191
HerzogKlaus <maxspace2000@mail.ru>
2023-07-08 18:07:58 +0500

    Добавил babel, добавил extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,41 +9,62 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
+  const babelLoader = {
+    test: /\.(js|ts|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true
+            }
+          ]
+        ]
+      }
+    }
+  }
+
   const lessLoader = {
-      test: /\.less$/i,
-      use: [
-        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-        {
-          loader: "css-loader",
-          options: {
-            modules: {
-              auto: (resPath: string) => (/\.module\./g).test(resPath),
-              localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
-            },
-          }
-        },
-        "less-loader",
-      ],
-    }
+    test: /\.less$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => (/\.module\./g).test(resPath),
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+          },
+        }
+      },
+      "less-loader",
+    ],
+  }
 
-    const svgLoader = {
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    }
+  const svgLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: ['@svgr/webpack'],
+  }
 
-    const fileLoader = {
-      test: /\.(png|jpe?g|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    }
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
 
   return [
     svgLoader,
     fileLoader,
+    babelLoader,
     typescriptLoader,
     lessLoader
   ]
